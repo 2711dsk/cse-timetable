e35814cbe8a81b704fc5f541a5d4f249977a1aec
2711dsk <saikrishnadurvasula@gmail.com>
2025-04-12 10:11:19 +0530

    Docker-4

--- server/connection.js
+++ server/connection.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "host.docker.internal",
     user: "root",
     password: "",
     database: "timetable_cse"
